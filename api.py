c451fe75a300b52c9e178f2559dd7b1861bf538c
imani-prog <timothyimani6@gmail.com>
2025-04-19 12:17:40 +0300

    Added wikipedia.py

--- api.py
+++ api.py
@@ -9,3 +9,11 @@ r = requests.get(url)
 
 # Print the text of the response
 print(r.text)
+
+
+import requests
+url = 'http://www.omdbapi.com/?apikey=72bc447a&t=social+network'
+r = requests.get(url)
+json_data = r.json()
+for k in json_data.keys():
+    print(k + ': ', json_data[k])
